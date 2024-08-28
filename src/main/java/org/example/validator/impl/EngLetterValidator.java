5ae2b70c04421f98e1503d55b3f966828b1dd1ab
Nikita Karpenka <darknight1996.nk@gmail.com>
2024-08-28 20:30:25 +0300

    EngLetterValidator refactor

--- src/main/java/org/example/validator/impl/EngLetterValidator.java
+++ src/main/java/org/example/validator/impl/EngLetterValidator.java
@@ -5,8 +5,9 @@ import org.example.validator.Validator;
 public class EngLetterValidator implements Validator<Character> {
 
     @Override
-    public boolean validate(final Character value) {
-        return value >= 'a' && value <= 'z';
+    public boolean validate(final Character character) {
+        final char lowerCaseCharacter = Character.toLowerCase(character);
+        return lowerCaseCharacter >= 'a' && lowerCaseCharacter <= 'z';
     }
 
 }
